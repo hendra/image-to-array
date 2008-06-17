dd85b8449f3fc6a9d413d7ef524af09150d5ec52
hendra User <nicholash3ndra@gmail.com>
2008-06-17 17:47:04 +0700

    second updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   
   def upload
     redirect_to :action => :index if params[:uploaded_data].blank?
-    asset = Asset.new({:image => params[:uploaded_data]})
-    @color_codes = asset.convert_to_array_of_colours.map{|x| x.to_s(base=16)}
+    asset = Asset.new(params[:uploaded_data])
+    @color_codes = asset.to_colour_array
   end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,18 +1,17 @@
-class Asset < ActiveRecord::Base
+class Asset
   require 'RMagick'
   require 'tempfile'
   
-  def image=(incoming_file)
+  def initialize(incoming_file)
     @round_time = Time.now.to_i.to_s + "." + rand(100000).to_s + "."
     @file_name = @round_time + incoming_file.original_filename.gsub(/.(jpg|JPG|JPEG)/, '.jpeg')
     @original_filename = incoming_file.original_filename
     @content_type = incoming_file.content_type.chomp
     
     if !@content_type.include?('image')
-      @img = incoming_file.read    
+      raise "Is not an image"
     else
-      if incoming_file.kind_of?(Tempfile) || 
-         (RAILS_ENV == 'test' && incoming_file.kind_of?(ActionController::TestUploadedFile))
+      if incoming_file.kind_of?(Tempfile) || (RAILS_ENV == 'test' && incoming_file.kind_of?(ActionController::TestUploadedFile))
         @img = Magick::Image::read(incoming_file.local_path).first
       else
         @img = Magick::Image::from_blob(incoming_file.string).first
@@ -21,12 +20,8 @@ class Asset < ActiveRecord::Base
     end
   end
   
-  def convert_to_array_of_colours
-    if @content_type.include?('image')
-      return @img.export_pixels
-    else
-      return []
-    end
+  def to_colour_array
+      return @img.export_pixels.uniq
   end
   
 end
